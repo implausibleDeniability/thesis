c6548e5fbef1e82563d80e1735ce114a7fa2549c
Maxim Faleev <m.faleev@innopolis.university>
2023-02-06 19:41:54 +0300

    update bfs builder for correct start

--- bfs_builder.py
+++ bfs_builder.py
@@ -1,20 +1,27 @@
-from typing import Callable
+from typing import Callable, Iterable
 from bfs import Bfs
 from mongo_driver import MongoDriver
 
 
 class BfsBuilder:
+    MY_ID = 395182538
+
     def __init__(self, mongo_driver: MongoDriver):
         self.mongo_driver = mongo_driver
 
     def build(self, get_neighbors: Callable) -> Bfs:
+        queue, visited = self.load_state_from_mongo()
+        bfs = Bfs(self.MY_ID, get_neighbors)
+        if len(visited) == 0:
+            bfs.load_state(queue, visited)
+        return bfs
+
+    def load_state_from_mongo(self) -> (Iterable[int], Iterable[int]):
         visited = set(self.mongo_driver.get_all_ids())
         queue = set()
         for node in self.mongo_driver.get_all_documents():
             queue.update(node['friends'])
         queue = list(queue.difference(visited))
-        bfs = Bfs(None, get_neighbors)
-        bfs.load_state(queue, visited)
-        return bfs
-        
+        return queue, visited
+
         
--- run_graph_topology_scraping.py
+++ run_graph_topology_scraping.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     api = Api()
     get_neighbors = api.get_friends
     mongo_driver = MongoDriver('topology')
-#     my_id = 395182538
-#     bfs = Bfs(my_id, get_neighbors)
     bfs = BfsBuilder(mongo_driver).build(get_neighbors)
     while True:
         user, friends = bfs.next()
