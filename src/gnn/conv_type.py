85f6bc666449d8fe78f4fcdef91007a5b0561b4e
Maxim Faleev <m.faleev@innopolis.university>
2023-05-26 22:24:19 +0300

    + from string converting

--- src/gnn/conv_type.py
+++ src/gnn/conv_type.py
@@ -4,3 +4,16 @@ class ConvType(Enum):
     gat = 2
     res = 3
     gin = 4
+
+    @classmethod
+    def from_string(cls, conv_type: str) -> ConvType:
+        if conv_type == 'gcn':
+            return cls.gcn
+        elif conv_type == 'gat':
+            return cls.gat
+        elif conv_type == 'res':
+            return cls.res
+        elif conv_type == 'gin':
+            return cls.gin
+        else:
+            raise NotImplementedError()
--- src/metrics/metrics.py
+++ src/metrics/metrics.py
@@ -4,3 +4,14 @@ class Metrics(Enum):
     balanced_accuracy = 0
     columnwise_balanced_accuracy = 1
     map_at_k = 2
+
+    @classmethod
+    def from_string(cls, metric: str) -> Metrics:
+        if metric == "balanced_accuracy":
+            return cls.balanced_accuracy
+        elif metric == "columnwise_balanced_accuracy":
+            return cls.columnwise_balanced_accuracy
+        elif metric == "map_at_k":
+            return cls.map_at_k
+        else:
+            raise NotImplementedError()
